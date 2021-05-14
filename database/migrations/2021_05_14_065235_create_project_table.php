e33d4ef2559c8c242674c543c1aa0241d856694f
Nathaly Sipiran <nsipiran@unitru.edu.pe>
2021-05-14 01:58:14 -0500

    create tables whit migration php artisan make:migration create_*nametable*_table

---
+++ database/migrations/2021_05_14_065235_create_project_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project', function (Blueprint $table) {
+            $table->id('project_id');
+            $table->unsignedBigInteger('city_id');
+            $table->unsignedBigInteger('company_id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('name');
+            $table->date('execution_date')->nullable();
+            $table->tinyInteger('is_active')->nullable();
+            //foreans key
+
+            $table->foreign('city_id')->references('city_id')->on('cities');
+            $table->foreign('company_id')->references('company_id')->on('companies');
+            $table->foreign('user_id')->references('user_id')->on('users');
+
+
+
+
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project');
+    }
+}
